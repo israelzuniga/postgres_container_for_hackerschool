844b3c1a2ec5b58fa44b7a0f4cb6d0a4188c7b93
Israel Zúñiga de la Mora <israelzuniga@users.noreply.github.com>
2016-06-17 18:19:30 -0500

    Created files and basic setup

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+#
+
+FROM postgres:9:5
+ENV LANG en_US.utf8
+
+MANTAINER Israel Zúñiga de la Mora <docker@israelzuniga.com>
+
+ADD remote_connections_setup.sh /docker-entrypoint-initdb.d/remote_connections_setup.sh
+RUN chmod 755 /docker-entrypoint-initdb.d/remote_connections_setup.sh
+ADD init-user-db.sh /docker-entrypoint-initdb.d/init-user-db.sh
+RUN init-user-db.sh /docker-entrypoint-initdb.d/init-user-db.sh
+ADD dvdrental.tar
+RUN pg_restore -U postgres -d dvdrental dvdrental.tar
---
+++ init-user-db.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+TEST=`gosu postgres postgres --single <<- EOSQL
+	SELECT 1 FROM pg_database WHERE datname='$DB_NAME';
+EOSQL`
+
+echo ""
+if [[ $TEST == "1" ]]; then
+	 # database exists
+	 # $? is 0
+	 exit 0
+else
+gosu postgres postgres --single <<- EOSQL
+	CREATE ROLE hschool WITH LOGIN ENCRYPTED PASSWORD 3raclase CREATEDB;
+EOSQL
+
+gosu postgres postgres --single <<- EOSQL
+	CREATE DATABASE dvdrental WITH OWNER hschool TEMPLATE template0 ENCODING 'UTF8';
+EOSQL
+
+gosu postgres postgres --single <<- EOSQL
+	GRANT ALL PRIVILEGES ON DATABASE dvdrental TO hschool;
+EOSQL
+fi
+
+echo ""
+echo "******DATABASE "dvdrental" CREATED******"
